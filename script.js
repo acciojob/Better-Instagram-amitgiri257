58bd65ef8d46da4690c7660d0c8926eed69fa8cb
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-09-24 20:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+$(document).ready(function() {
+    $(".image").draggable({
+        helper: "clone",
+        revert: "invalid"
+    });
+
+    $(".image").droppable({
+        accept: ".image",
+        drop: function(event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
+
+            // Swap background images
+            var temp = draggable.css("background-image");
+            draggable.css("background-image", droppable.css("background-image"));
+            droppable.css("background-image", temp);
+        }
+    });
+});
